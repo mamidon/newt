7d7a0798963aec5bafbaa2831eb6d360252935a9
mamidon <mamidon@outlook.com>
2019-04-09 21:50:32 -0700

    Make final fixups for the token approvals.. there was actually a lot

--- tools/approve.ps1
+++ tools/approve.ps1
@@ -1,5 +1,18 @@
 #!/usr/local/bin/pwsh
 
+function Replace-Suffix {
+	param (
+		[string]
+		$text,
+		[string]
+		$oldSuffix,
+		[string]
+		$newSuffix
+	)
+
+	"$($text.Replace($oldSuffix, """"))$newSuffix"
+}
+
 function Approve-Files {
 	param(
 		[string]
@@ -8,40 +21,40 @@ function Approve-Files {
 		$approvedSuffix
 	)
 
-	function Replace-Suffix {
-		param (
-			[string]
-			$input,
-			[string]
-			$oldSuffix,
-			[string]
-			$newSuffix
-		)
-
-		"$($input.Replace($oldSuffix, ''))$newSuffix"
-	}
-
 	Get-ChildItem -Recurse . | ? {
 		$_.Name.EndsWith($candidateSuffix) 
 	} | % {
-		touch $(Replace-Suffix($_.Name, $candidateSuffix, $approvedSuffix))
+		$approvedFilePathName = $(Replace-Suffix -text ($_.FullName) -oldSuffix $candidateSuffix -newSuffix $approvedSuffix)
+		touch $approvedFilePathName
+	
 		$_
 	} | ? {
-		(Compare-Object (Get-Content $_.Name) (Get-Content (Replace-Suffix($_.Name, $candidateSuffix, $approvedSuffix)))).Count -gt 0
+		$approvedFilePathName = $(Replace-Suffix -text ($_.FullName) -oldSuffix $candidateSuffix -newSuffix $approvedSuffix)
+		$candidateContent = Get-Content $_.FullName
+		$approvedContent = Get-Content $approvedFilePathName
+
+		if ($null -eq $approvedContent) {
+			$approvedContent = ""
+		}
+
+		$delta = (Compare-Object -Ref $candidateContent $approvedContent -PassThru).Count
+		
+		$delta -gt 0
 	} | % {
-		diffmerge $_.Name (Replace-Suffix($_.Name, $candidateSuffix, $approvedSuffix))
+		$approvedFilePathName = $(Replace-Suffix -text ($_.FullName) -oldSuffix $candidateSuffix -newSuffix $approvedSuffix)
+		diffmerge $_.FullName $approvedFilePathName
 	}
 }
 
 function Approve-Tokens {
 	$inputSuffix = ".tokens.newt"
 	$outputSuffix = ".tokens.approval"
-	$approvedSuffix = ".token.approved"
+	$approvedSuffix = ".tokens.approved"
 
-	Get-ChildItem -Recurse . | ? {
-		$_.Name.EndsWith($inputSuffix) 
-	} | % {
-		& cargo run -- --entry-file $_.Name --output-mode tokens
+	Get-ChildItem -Recurse -Filter "*.tokens.newt" . | % {
+		$outputFile = Replace-Suffix -text ($_.FullName) -oldSuffix $inputSuffix -newSuffix $outputSuffix
+
+		& cargo run --quiet -- --entry-file $_.FullName --output-mode tokens > $outputFile
 	}
 
 	Approve-Files -candidateSuffix $outputSuffix -approvedSuffix $approvedSuffix
