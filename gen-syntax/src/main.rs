8211232608fe7b1f1f17808bd02eadaa372f489f
mamidon <mamidon@outlook.com>
2019-09-09 21:01:59 -0700

    Some comments to guide myself later

--- gen-syntax/src/main.rs
+++ gen-syntax/src/main.rs
@@ -14,6 +14,21 @@ use std::io::{self, Read};
 
 extern crate ansi_term;
 
+/*
+TODO -- actually deliver value now
+
+1. If any errors are found, print them out and stop
+2. Gather up the distinct set of node types to create the top level SyntaxKind enum
+2. Wherever there is a pipe operator, create a sub-kind.  e.g. Expr & Stmt
+-- it is illegal for any there to be multiple identifiers in a pipe branch, we won't
+-- know what to name it's relevant kind et al
+3. Wherever there is a * emit no methods
+
+TODO -- later allow for the specification of a set of tokens e.g. "{" "(" "." etc
+TODO -- any tokens referenced not in the set are treated as undefined symbols, but otherwise
+TODO -- they have no effect on the output
+TODO -- once that's done, dogfood on this crate
+*/
 fn main() -> io::Result<()> {
 	let mut buffer = String::new();
 
