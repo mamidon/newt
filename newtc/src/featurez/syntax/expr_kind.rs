ab0c44ca9d5004d1d6fed08a4360d3ae2ce4542b
mamidon <mamidon@outlook.com>
2019-11-27 07:21:58 -0800

    Add basic support for object literals in other places

--- newtc/src/featurez/syntax/expr_kind.rs
+++ newtc/src/featurez/syntax/expr_kind.rs
@@ -4,6 +4,7 @@ pub enum ExprKind<'a> {
     BinaryExpr(&'a BinaryExprNode),
     UnaryExpr(&'a UnaryExprNode),
     PrimitiveLiteralExpr(&'a PrimitiveLiteralExprNode),
+    ObjectLiteralExpr(&'a ObjectLiteralExprNode),
     GroupingExpr(&'a GroupingExprNode),
     VariableExpr(&'a VariableExprNode),
     FunctionCallExpr(&'a FunctionCallExprNode),
