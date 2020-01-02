ef66273fedd04159c3754f65517e32a4a73cea85
mamidon <mamidon@outlook.com>
2020-01-02 07:45:17 -0800

    holding

--- host/src/main.rs
+++ host/src/main.rs
@@ -6,112 +6,122 @@ extern crate vulkano_win;
 use vulkano::instance::{Instance, PhysicalDevice, QueueFamily};
 use winit::{EventsLoop, WindowBuilder, Window};
 use vulkano_win::VkSurfaceBuild;
-use vulkano::device::{DeviceExtensions, Device, DeviceCreationError};
+use vulkano::device::{DeviceExtensions, Device, DeviceCreationError, QueuesIter};
 use std::sync::Arc;
 use vulkano::swapchain::Surface;
 use std::fmt::{Display, Formatter, Error};
 
+struct VulkanState<'instance> {
+    instance: &'instance Arc<Instance>,
+    surface: Arc<Surface<Window>>,
+    physical_devices: Option<Vec<PhysicalDevice<'instance>>>,
+    selected_physical_device: Option<PhysicalDevice<'instance>>,
+}
+
+impl<'instance> VulkanState<'instance> {
+    pub fn new(instance: &'instance Arc<Instance>, surface: &Arc<Surface<Window>>) -> VulkanState<'instance> {
+        VulkanState {
+            instance,
+            surface: surface.clone(),
+            physical_devices: None,
+            selected_physical_device: None
+        }
+    }
+}
+
 struct VulkanStateBuilder<'instance> {
-    instance: Arc<Instance>,
-    devices: Vec<PhysicalDevice<'instance>>,
-    queue_families: Option<Vec<QueueFamily<'instance>>>,
-    selected_device: Option<PhysicalDevice<'instance>>,
-    selected_queue_family: Option<QueueFamily<'instance>>,
-    surface: Option<Arc<Surface<Window>>>,
+    state: VulkanState<'instance>,
+    device_selector: Option<fn(&PhysicalDevice) -> bool>,
+    queue_family_selector: Option<fn(&QueueFamily, &Arc<Surface<Window>>) -> bool>,
     logical_device: Option<Arc<Device>>
 }
 
 impl<'instance> VulkanStateBuilder<'instance> {
-    pub fn new(instance: &'instance Arc<Instance>) -> VulkanStateBuilder<'instance> {
+    pub fn new(state: VulkanState<'instance>) -> VulkanStateBuilder {
         VulkanStateBuilder {
-            instance: instance.clone(),
-            devices: PhysicalDevice::enumerate(instance).collect(),
-            queue_families: None,
-            surface: None,
-            selected_device: None,
-            selected_queue_family: None,
+            state,
+            device_selector: None,
+            queue_family_selector: None,
             logical_device: None
         }
     }
 
-    pub fn pick_device<P>(&mut self, predicate: P)
-        where P: Fn(&PhysicalDevice) -> bool
+    pub fn set_device_selector(&mut self, predicate: fn(&PhysicalDevice) -> bool)
     {
-        self.selected_device = self.devices.iter()
-            .find(|device| predicate(device))
-            .map(|device| device.clone());
+        self.device_selector = Some(predicate);
+    }
 
-        if let Some(device) = self.selected_device {
-            self.queue_families = Some(device.queue_families().collect());
+    pub fn set_queue_family_selector(&mut self, predicate: fn(&QueueFamily, &Arc<Surface<Window>>) -> bool)
+    {
+        self.queue_family_selector = Some(predicate);
+    }
+
+    fn get_devices(&self) -> Vec<PhysicalDevice> {
+        if let Some(devices) = self.state.physical_devices.as_ref() {
+            return devices.clone();
+        }
+
+        let devices: Vec<_> = PhysicalDevice::enumerate(&self.state.instance).collect();
+        self.state.physical_devices
+            .replace(devices);
+
+        self.state.physical_devices
+            .expect("Given an Instance, polling the devices shouldn't fail")
+            .clone()
+    }
+
+    fn get_device_selector(&self) -> &fn(&PhysicalDevice) -> bool {
+        match self.device_selector.as_ref() {
+            Some(selector) => selector,
+            None => unimplemented!()
         }
     }
 
-    pub fn pick_queue_family<P>(&mut self, predicate: P)
-        where P: Fn(&QueueFamily) -> bool
-    {
-        self.selected_queue_family = self.queue_families
-            .as_ref()
-            .and_then(|families| families.iter().find(|family| predicate(family)))
-            .map(|family| family.clone());
+    fn get_queue_family_selector(&self) -> &fn(&QueueFamily, &Arc<Surface<Window>>) -> bool {
+        match self.queue_family_selector.as_ref() {
+            Some(selector) => selector,
+            None => unimplemented!()
+        }
     }
 
-    pub fn initialize_logical_device(&mut self) -> Result<(), DeviceCreationError>{
-        let device = self.selected_device
-            .or_else(self.pick_device(|d| true))
-            .expect("Couldn't find a physical device!");
-        let queue_family = self.selected_queue_family
-            .or_else(self.pick_queue_family(|qf| true))
-            .expect("Couldn't find a queue family!");
+    fn get_selected_device(&self) -> Result<PhysicalDevice, String> {
+        let selector = self.get_device_selector();
+
+        self.get_devices()
+            .into_iter()
+            .find(|device| selector(device))
+            .map(|device| device)
+            .ok_or("PhysicalDevice not found".to_string())
+    }
+
+    fn get_queue_families(&self) -> Result<Vec<QueueFamily>, String> {
+        let selected_device = self.get_selected_device()?;
+
+        Ok(selected_device.queue_families().collect())
+    }
+
+    fn get_selected_queue_family(&self) -> Result<QueueFamily, String> {
+        let queue_family_selector = self.get_queue_family_selector();
+        let selected_queue_family = self.get_queue_families()?
+            .into_iter()
+            .find(|qf| queue_family_selector(qf, &self.state.surface));
+
+        selected_queue_family
+            .ok_or("Failed to select a QueueFamily".to_string())
+    }
+
+    pub fn get_logical_device(&self) -> Result<(Arc<Device>, QueuesIter), String>{
+        let device = self.get_selected_device()?;
+        let queue_family = self.get_selected_queue_family()?;
 
         let required_extensions = DeviceExtensions { khr_swapchain: true, .. DeviceExtensions::none() };
-        self.logical_device = Some(Device::new(device,
+        let device_result = Device::new(device,
                     device.supported_features(),
                     &required_extensions,
-                    [(queue_family, 0.5)].iter().cloned())?.0);
+                    [(queue_family, 0.5)].iter().cloned());
 
-        Ok(())
-    }
-}
-
-impl<'instance> Display for VulkanStateBuilder<'instance> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        writeln!(f, "Instance: {:?}", self.instance)?;
-        writeln!(f)?;
-
-        writeln!(f, "Loaded Extensions: {:?}", self.instance.loaded_extensions())?;
-
-        if self.devices.is_empty() {
-            writeln!(f, "Devices: None")?;
-        } else {
-            writeln!(f, "Devices:")?;
-            for device in &self.devices {
-                writeln!(f, "\t{:?}", device)?;
-            }
-        }
-
-        writeln!(f, "Selected Device: {:?}", self.selected_device);
-        writeln!(f)?;
-
-        match &self.queue_families {
-            Some(queue_families) => {
-                writeln!(f, "Queue Families:");
-                for queue_family in self.queue_families.as_ref().unwrap_or(&vec![]).iter() {
-                    let id = queue_family.id();
-                    let graphics = queue_family.supports_graphics();
-                    let compute = queue_family.supports_compute();
-                    let surface = /* TODO */
-                    writeln!(f, "\tId: {}  Graphics?: {}  Compute?: {}  Window?: {}", queue_family)?;
-                }
-            }
-            None => {
-                writeln!(f, "Queue Families: None")?;
-            }
-        }
-
-        writeln!(f, "Selected Queue Family: {:?}", self.selected_queue_family.map(|qf| qf.id()));
-        writeln!(f)?;
-
-        Ok(())
+        device_result.ok()
+            .ok_or("Failed to create a logical Device".to_string())
     }
 }
 
@@ -119,21 +129,24 @@ fn main() {
     let instance = Instance::new(
         None,
         &vulkano_win::required_extensions(),
-        None).unwrap();
-    let mut builder = VulkanStateBuilder::new(&instance);
+        None)
+        .unwrap();
 
     let events_loop = EventsLoop::new();
     let surface = WindowBuilder::new()
         .build_vk_surface(&events_loop, instance.clone())
         .unwrap();
 
-    builder.pick_device(|_| true);
-    builder.pick_queue_family(|family| {
-        println!("queue: {} {}", family.supports_graphics(), surface.is_supported(*family).unwrap_or(false));
+    let mut state = VulkanState::new(&instance, &surface);
+    let mut builder = VulkanStateBuilder::new(state);
 
+    builder.set_device_selector(|_| true);
+    builder.set_queue_family_selector(|family, surface|
         family.supports_graphics() && surface.is_supported(*family).unwrap_or(false)
-    });
-    builder.initialize_logical_device();
+    );
+    let (logical_device, mut queues) = builder.get_logical_device().unwrap();
+    let logical_queues: Vec<_> = queues.collect();
 
-    println!("{}", builder);
+    println!("{:?}", logical_device);
+    println!("{:?}", logical_queues);
 }
