206adaf798e3c0f940eb38c5235370dd700d3e07
mamidon <mamidon@outlook.com>
2020-02-18 17:43:37 -0800

    Delete unused code (yay!)

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,40 +1,12 @@
 #![allow(unused)]
 
 extern crate drawing;
-extern crate vulkano;
-extern crate vulkano_win;
 
-use crate::newt_render::attachments::HostSurface;
-use crate::newt_render::{RenderCommand, Renderer};
 use drawing::{Brush, DrawCommand, Drawing, DrawingOptions, Extent, ShapeKind};
 use png;
-use std::cell::RefCell;
-use std::fmt::{Display, Error, Formatter};
 use std::io::Cursor;
-use std::sync::Arc;
-use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
-use vulkano::command_buffer::{AutoCommandBufferBuilder, DynamicState};
-use vulkano::device::{Device, DeviceCreationError, DeviceExtensions, Queue, QueuesIter};
-use vulkano::framebuffer::{Framebuffer, FramebufferAbstract, RenderPassAbstract, Subpass};
-use vulkano::image::{Dimensions, ImmutableImage, SwapchainImage};
-use vulkano::instance::{Instance, PhysicalDevice, QueueFamily};
-use vulkano::pipeline::viewport::Viewport;
-use vulkano::pipeline::GraphicsPipeline;
-use vulkano::swapchain::{
-    AcquireError, ColorSpace, PresentMode, Surface, SurfaceTransform, Swapchain,
-    SwapchainCreationError,
-};
-use vulkano::sync::{now, FlushError, GpuFuture};
-use vulkano_win::VkSurfaceBuild;
+
 use winit::{Event, EventsLoop, Window, WindowBuilder, WindowEvent};
-
-#[derive(Default, Debug, Clone)]
-struct Vertex {
-    position: [f32; 2],
-}
-vulkano::impl_vertex!(Vertex, position);
-
-mod newt_render;
 
 fn main() {
     let mut events_loop = EventsLoop::new();
@@ -50,10 +22,6 @@ fn main() {
     let image = include_bytes!("image_img.png").to_vec();
     let decoder = png::Decoder::new(Cursor::new(image));
     let (info, mut reader) = decoder.read_info().unwrap();
-    let image_dimensions = Dimensions::Dim2d {
-        width: info.width,
-        height: info.height,
-    };
     let mut image_data = Vec::new();
     image_data.resize((info.height * info.width * 4) as usize, 0);
     reader.next_frame(&mut image_data).unwrap();
